44b8477dc563b2b4a28a1b1d1b08b81b650e36dd
Ceddy Muhoza <ceddymuhoza@gmail.com>
2020-04-17 18:20:10 -0400

    gnuplot

--- src/dev.ts
+++ src/dev.ts
@@ -7,6 +7,6 @@ plot({
     data: [3, 1, 2, 3, 4],
     filename: __dirname + '/test/outputXXX.png',
     format: 'png',
-    style: 'boxes',
+    style: 'boxes lc rgb "red"',
     args: ['set boxwidth 0.5', 'set style fill solid']
 });
